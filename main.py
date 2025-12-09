5ddd6ba9afa0b2f824adafed1ad3b4f8646ede6b
moon-may <katrin-moonmay@yandex.ru>
2025-12-09 03:40:28 +0300

    Доработка интерфейса

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from tkinter.scrolledtext import ScrolledText
 from tkinter import ttk
 from tensorflow import keras
 import pandas as pd
-import numpy as np
 
-# Загрузка модели
+'''
+Загрузка модели
+Автор: Стасевич Екатерина Алексеевна
+Тема ВКР: Прогнозирование количества отчисленных студентов с помощью методов машинного обучения
+'''
 model = keras.models.load_model('model.keras')
 
 # Логика работы приложения
@@ -41,15 +44,10 @@ def predict():
     risk_group = result[result['result'] == 1].sort_values('predicts', ascending=False)
 
     result_entry.delete('1.0', 'end')
-    result_entry.insert('end', 'Студенты в группе риска:\n')
+    result_entry.insert('end', 'Студенты в группе риска:\n\n')
 
     for _, i in risk_group.iterrows():
-        result_entry.insert('end', f"{i['id_student']}, {i['predicts']:.1%}\n")
-
-
-
-
-
+        result_entry.insert('end', f"{i['id_student']}, вероятность отчисления: {i['predicts']:.1%}\n")
 
 
 # Интерфейс приложения
@@ -69,16 +67,19 @@ main_frame.pack(side='left', fill='both', expand=True)
 result_frame = Frame(root, width=400, height=500)
 result_frame.pack(side='right', fill='both', expand=True)
 
+# Подпись для окошка ввода имени файла
 istr_label = ttk.Label(main_frame,
                    text='Загрузите данные для анализа',
                    font=('Arial', 14))
 istr_label.pack(anchor='w', pady=10, padx=10)
 
+# Окно ввода имени файла
 data_entry = ttk.Entry(main_frame, font='20', width=50)
 data_entry.pack(padx=10, pady=10)
 
-pred_btn = ttk.Button(main_frame, text='Рассчитать',
-                  command=predict)
+# Кпопка для расчета
+pred_btn = Button(main_frame, text='Рассчитать', font=20,
+                  command=predict) # привязка к функции
 pred_btn.pack()
 
 # Переменные для вывода результата количеством и в процентах
@@ -86,11 +87,14 @@ total = 100     # общее количество
 dropout = 20    # количество в группе риска
 percent = dropout / total * 100 # процент
 
+# Блок для вывода результата
 result_label = ttk.Label(main_frame, text='',
                          font=('Arial', 14))
 result_label.pack(anchor='w', pady=10, padx=10)
 
-result_entry = ScrolledText(result_frame, width=400, height=500)
+# Поле вывода списка студентов с результатами
+result_entry = ScrolledText(result_frame, width=400, height=500,
+                            font=('Arial', 14))
 result_entry.pack(anchor='w', pady=10, padx=10)
 
 root.mainloop()
